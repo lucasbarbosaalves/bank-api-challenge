bf10e2406c7bd172cc4cc1d436ab953587a26443
lucas.barbosa <lkab05@hotmail.com>
2025-10-16 15:29:44 -0300

    retrieve account id on user response dto

--- src/main/java/com/lucasbarbosaalves/bank/entities/users/UserResponseDTO.java
+++ src/main/java/com/lucasbarbosaalves/bank/entities/users/UserResponseDTO.java
@@ -6,9 +6,10 @@ public record UserResponseDTO(
         UUID id,
         String name,
         String email,
+        UUID accountId,
         Long accountNumber
 ) {
     public UserResponseDTO(User user) {
-        this(user.getId(), user.getName(), user.getEmail(), user.getAccount().getAccountNumber());
+        this(user.getId(), user.getName(), user.getEmail(), user.getAccount().getId(), user.getAccount().getAccountNumber());
     }
 }
